f3e711c9cce2c35cbac6f417e1127751d0ee10ac
fpwking <92611818+fpwking@users.noreply.github.com>
2024-04-24 21:26:09 -0400

    Works
    Works.
    
    Optimize later

--- Radio/Recieve.py
+++ Radio/Recieve.py
@@ -1,14 +1,25 @@
 from radio import Radio
-import serial
+import time
+import bitstring
 
-# sup = serial.Serial('COM5', 9600, timeout=0.1)
-# print(sup.readline().decode())
-# sup.write(bytes("AT\r\n", 'utf-8'))
-# print(sup.readline().decode())
-# sup.close()
+GROUND = Radio('COM5', 9600, 0.1)
+CANSAT = Radio('COM7', 9600, 0.1)
 
-GROUND = Radio('COM5', 9600, timeout=0.1)
-CANSAT = Radio('COM7', 9600, timeout=0.1)
+def setup():
+    print("Ground")
+    GROUND.radioSetup(10, 7, 1, 7, 915000000, 1, 1, None, 10)
+    print("Cansat")
+    CANSAT.radioSetup(10, 7, 2, 7, 915000000, 2, 1, None, 10)
 
-GROUND.sendThis()
+
+CANSAT.readAll()
+GROUND.send(2, " 7 bits (humid Xle of 8 bits can be sent. If you need to send 17 bits for example, 3 bytes are needed: 2 for the first 16 bits and one for the last bit (the remaining 7 bits can be reserved for further use).")
+time.sleep(2)
+CANSAT.read()
+GROUND.send(2, "Convert to bits")
+time.sleep(1)
 GROUND.read()
+CANSAT.read()
+
+GROUND.close()
+CANSAT.close()
--- Radio/radio.py
+++ Radio/radio.py
@@ -1,19 +1,101 @@
-from serial import Serial
+import serial
 import sys
+import time
 
-class Radio(Serial):
-    def __innit__(self):
-         super().__init__()
+class Radio():
+    def __init__(self, port, baudrate, timeOut):
+        self.__PORT = port
+        self.__BAUDRATE = baudrate
+        self.__TIMEOUT = timeOut
+        self.__RADIO = serial.Serial(self.__PORT, self.__BAUDRATE, timeout = self.__TIMEOUT)      
 
-    def sendThis (self):
-        self.write(bytes("AT\r\n", 'utf-8'))
+    def sendSelf(self, x):
+        self.__RADIO.write(bytes(x+"\r\n", 'utf-8'))
 
-    def send (self, address, x):
-            msg = bytes(x, 'utf-8')
-            self.write("AT+SEND=" + address + "," + sys.getsizeof(msg) + "," + msg + "\r\n")
+    def send(self, address, x):
+            self.__RADIO.write(bytes("AT+SEND=%d,%d,%s\r\n" %(address, sys.getsizeof(x)-49, x), 'utf-8'))
     
     def read(self):
-        print(self.readline().decode())
+        print(self.__RADIO.readline().decode())
+
+    def readAll(self):
+        print(self.__RADIO.read_all().decode())
+    
+    def close(self):
+         self.__RADIO.close()
+
+    # Initiallizing the radio
+    def reset(self):
+         self.sendSelf("AT+RESET")
+    
+    # <Spreading Factor> Larger SF = better sensitivity and longer transmission time
+    # <Bandwidth> Smaller BW = better sensitivity and longer transmission time
+    # <Coding Rate> Determines the redundant bits. Lower CR = faster transmission time
+    # <Programmed Preamble> Larger PP = less loss of data and longer transmission time
+    # 
+    # Recommanded:
+    # Within 3 km: (10, 7, 1, 7)
+    # More than 3 km: (12, 4, 1, 7)
+    def setParam(self, sf, bw, cr, pp):
+         self.__SF = sf
+         self.__BW = bw
+         self.__CR = cr
+         self.__PP = pp
+         self.sendSelf("AT+PARAMETER=%d,%d,%d,%d" %(self.__SF, self.__BW, self.__CR, self.__PP))
+         
+    # <Frequency> 915MHz in Canada
+    def setFRQ(self, fq):
+         self.__FQ = fq
+         self.sendSelf("AT+BAND=%d" %self.__FQ)
+
+    # <Address> Address of the radio module. 0~65535
+    def setADR(self, adr):
+         self.__ADR = adr
+         self.sendSelf("AT+ADDRESS=%d" %self.__ADR)
+
+    # <Network> Public network of LoRa. Radios must be in same group to communicate. 0~16
+    def setNTW(self, ntw):
+         self.__NTW = ntw
+         self.sendSelf("AT+NETWORKID=%d" %self.__NTW)
+
+    #<Password> Must have same password for data to be recognized. 32 character AES password.
+    def setPASS(self, PASS):
+         self.__PASS = PASS
+         if self.__PASS is None: return 
+         else: self.sendSelf("AT+CPIN=%s" %self.__PASS)
+
+    #<Output Power>
+    def setOP(self, op):
+         self.__OP = op
+         self.sendSelf("AT+CRFOP=%d" %self.__OP)
+
+    # Batch initialize
+    def radioSetup(self, SF, BW, CR, PP, FQ, ADR, NTW, PASS, OP):
+         self.setParam(SF, BW, CR, PP)
+         time.sleep(0.1)
+         self.read()
+
+         self.setFRQ(FQ)
+         time.sleep(0.1)
+         self.read()
+
+         self.setADR(ADR)
+         time.sleep(0.1)
+         self.read()
+
+         self.setNTW(NTW)
+         time.sleep(0.1)
+         self.read()
+
+         self.setPASS(PASS)
+         time.sleep(0.1)
+         self.read()
+
+         self.setOP(OP)
+         time.sleep(0.1)
+         self.read()
+
+
 
 
     
